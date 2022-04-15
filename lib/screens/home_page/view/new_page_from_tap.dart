83a65365f42bb9d9b934726225f6af03a0c4476e
i419 <bealoneasif@gmail.com>
2022-04-16 03:16:25 +0530

    fix: added modified files to changelist - "Cards redirect to New pages"

---
+++ lib/screens/home_page/view/new_page_from_tap.dart
@@ -0,0 +1,38 @@
+import 'package:calorie_tracker/screens/home_page/models/data.dart';
+import 'package:flutter/material.dart';
+
+class NewPage extends StatelessWidget {
+  final CardItem item;
+  const NewPage({Key? key,
+    required this.item
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item.time),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 4 / 3,
+            child: Image.network(item.urlImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            item.title,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight:FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
