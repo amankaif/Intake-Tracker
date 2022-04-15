094f695dc351bf505c2330f45b08db049135beec
i419 <bealoneasif@gmail.com>
2022-04-15 20:06:06 +0530

    "Cards redirect to New pages"

--- lib/screens/home_page/view/home_screen.dart
+++ lib/screens/home_page/view/home_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:calorie_tracker/screens/home_page/home_page.dart';
 import 'package:calorie_tracker/ui_strings.dart';
@@ -39,6 +40,48 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
   @override
   Widget build(BuildContext context) {
+
+    Widget buildCard({
+      required CardItem item,
+    }) {
+      return Container(
+        width: 200,
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: AspectRatio(
+                      aspectRatio: 4 / 3,
+                      child: Material(
+                        child: Ink.image(
+                          image: NetworkImage(item.urlImage),
+                          fit: BoxFit.cover,
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => NewPage(
+                                    item: item,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                  )),
+            ),
+            const SizedBox(height: 4),
+            Text(item.title,
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text(item.time,
+                style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      );
+    }
     return Scaffold(
       drawer: const NavBar(),
       appBar: AppBar(
@@ -47,6 +90,7 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0.0,
         // backgroundColor: Colors.green,
       ),
+
       body: Container(
           height: 200,
           padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
@@ -65,5 +109,6 @@ class _HomeScreenState extends State<HomeScreen> {
         // backgroundColor: Colors.green,
       ),
     );
+
   }
 }
--- lib/ui_strings.dart
+++ lib/ui_strings.dart
@@ -5,5 +5,5 @@ var displayedDayDateString = const Text("Monday 17 April, 2022",
       // fontFamily: 'Inter',
       fontSize: 17,
     ));
-final time_now = DateTime.now();
-final time_parse = DateTime.parse('1969-07-20 20:18:04Z');
+final timeNow = DateTime.now();
+final timeParse = DateTime.parse('1969-07-20 20:18:04Z');
