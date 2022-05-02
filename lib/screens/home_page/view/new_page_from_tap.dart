ce65bb65d2e25039065c9feb4d1b9387cc1625b7
Aman Kaif <aman220202@gmail.com>
2022-05-02 20:04:09 +0530

    minor: removed unnecessary container

--- lib/screens/home_page/view/new_page_from_tap.dart
+++ lib/screens/home_page/view/new_page_from_tap.dart
@@ -22,82 +22,80 @@ class NewPage extends StatelessWidget {
         title: Text(item.time),
         centerTitle: true,
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            // children: [CardDemoMeals(), CardDemoMeals(), CardDemoMeals()]
-            // children: breakfast.map((meals) => mealsCard(meals)).toList(),
-            children: [
-              AspectRatio(
-                aspectRatio: 5 / 3,
-                child: Image.asset(
-                  item.urlImage,
-                  fit: BoxFit.cover,
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          // children: [CardDemoMeals(), CardDemoMeals(), CardDemoMeals()]
+          // children: breakfast.map((meals) => mealsCard(meals)).toList(),
+          children: [
+            AspectRatio(
+              aspectRatio: 5 / 3,
+              child: Image.asset(
+                item.urlImage,
+                fit: BoxFit.cover,
               ),
-              const SizedBox(height: 8),
-              Text(
-                item.title,
-                style: const TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              item.title,
+              style: const TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
               ),
-              Container(
-                height: 400,
-                padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-                child: FutureBuilder(
-                  future: dataBaseNotifier.fetchFooditems(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Row(
-                          children: <Widget>[CircularProgressIndicator()],
-                          mainAxisAlignment: MainAxisAlignment.center);
-                    }
-                    if (snapshot.hasData) {
-                      var _snapshot = snapshot.data as List;
-                      return ListView.separated(
-                        physics: const BouncingScrollPhysics(),
-                        scrollDirection: Axis.vertical,
-                        itemCount: _snapshot.length,
-                        separatorBuilder: (context, _) =>
-                            const SizedBox(height: 10),
-                        itemBuilder: (BuildContext context, int index) {
-                          FoodItems fooditems = _snapshot[index];
-                          return CardDemoMeals(
-                            name: fooditems.fName,
-                          );
-                        },
-                      );
-                    }
+            ),
+            Container(
+              height: 400,
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: FutureBuilder(
+                future: dataBaseNotifier.fetchFooditems(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
                     return Row(
                         children: <Widget>[CircularProgressIndicator()],
                         mainAxisAlignment: MainAxisAlignment.center);
-                  },
-                ),
+                  }
+                  if (snapshot.hasData) {
+                    var _snapshot = snapshot.data as List;
+                    return ListView.separated(
+                      physics: const BouncingScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      itemCount: _snapshot.length,
+                      separatorBuilder: (context, _) =>
+                          const SizedBox(height: 10),
+                      itemBuilder: (BuildContext context, int index) {
+                        FoodItems fooditems = _snapshot[index];
+                        return CardDemoMeals(
+                          name: fooditems.fName,
+                        );
+                      },
+                    );
+                  }
+                  return Row(
+                      children: <Widget>[CircularProgressIndicator()],
+                      mainAxisAlignment: MainAxisAlignment.center);
+                },
               ),
-              //   Expanded(
-              //     child: Container(
-              //       height: 350,
-              //       padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+            ),
+            //   Expanded(
+            //     child: Container(
+            //       height: 350,
+            //       padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
 
-              // child: ListView.separated(
-              //   physics: const BouncingScrollPhysics(),
-              //   scrollDirection: Axis.vertical,
-              //   itemCount: meals.length,
-              //   separatorBuilder: (context, _) =>
-              //       const SizedBox(height: 10),
-              //   itemBuilder: (BuildContext context, int index) =>
-              //       // CardDemoMeals(
-              //     meal: meals[index],
-              //     total: total,
-              //   ),
-              // ),
-              //     ),
-              //   ),
-            ],
-          ),
+            // child: ListView.separated(
+            //   physics: const BouncingScrollPhysics(),
+            //   scrollDirection: Axis.vertical,
+            //   itemCount: meals.length,
+            //   separatorBuilder: (context, _) =>
+            //       const SizedBox(height: 10),
+            //   itemBuilder: (BuildContext context, int index) =>
+            //       // CardDemoMeals(
+            //     meal: meals[index],
+            //     total: total,
+            //   ),
+            // ),
+            //     ),
+            //   ),
+          ],
         ),
       ),
     );
